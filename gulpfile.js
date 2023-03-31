7c482b18c6ce000d4190f51950ed55b0c80625a2
Francisco Martinez <fcomartinez94@gmail.com>
2023-03-31 13:19:28 -0700

    Dev tools agregadas

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const { src, dest, series } = require('gulp');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
+
+//Imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+function css( done ) {
+    src('src/css/*.css')
+        .pipe( sourcemaps.init() )
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe( sourcemaps.write('.'))
+        .pipe( dest('build/css') )
+
+    done();
+}
+
+function imagenes() {
+    return src('src/img/**/*')
+        .pipe(imagemin({ optimizationLevel: 3 }))
+        .pipe(dest('build/img'))
+}
+
+function versionWebp() {
+    const opciones = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+}
+
+function versionAvif() {
+    const opciones = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
+}
+
+// exports.default = series (imagenes, versionWebp, versionAvif, css)
+exports.default = css
